35cb06f95aa9d716556d183b2e0d22a480fc36a3
Seth Aker <saker132@gmail.com>
2023-09-18 19:11:12 -0500

    Finished for the day

--- src/views/MainMenu.vue
+++ src/views/MainMenu.vue
@@ -1,19 +1,22 @@
 <template>
    <main>
       <div class="menu-options">
-          <div class="new-game dropdown">
-            <a href="" @click.prevent="" >New Game</a>
+          <div class="menu-option new-game">
+              <div class="dropdown">
+                <button type="button" class="collapsable" @click="toggleNewGameActive">New Game</button>
             
-            <div class="dropdown-content">
-                <a href="" class="">Matching</a>
-                <a href="" >Blackjack</a>
-                <a href="" >Hearts</a>
-            </div>
+                <div class="dropdown-content" :class="{active: newGameActive}">
+                    <a href="" >Matching</a>
+                    <a href="" >Blackjack</a>
+                    <a href="" >Hearts</a>
+                </div>
+              </div>
+            
           </div>
-          <div class="about-me">
+          <div class="menu-option">
             <a href="https://www.linkedin.com/in/sethaker">About Me</a>
           </div>
-          <div class="source-code">
+          <div class="menu-option">
             <a href="https://github.com/seth-aker/Cardgames_v3">Source Code</a>
           </div>
       </div>
@@ -22,7 +25,17 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            newGameActive: false
+        }
+    },
+    methods: {
+        toggleNewGameActive() {
+            this.newGameActive = !this.newGameActive;
+        }
+    }
+    
 }
 </script>
 
@@ -36,53 +49,65 @@ main{
     flex-direction: column;
     align-items: center;
 }
-
-/* .dropdown:hover > a {
-    display: none;
-}
-
-.dropdown-content {
-    display: none;
-}
-
- .dropdown:hover .dropdown-content {
-    display: flex;
-    flex-direction: column;
-    transition: 400ms;
-} */
-.new-game{
+    
+.menu-option {
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     width: 300px;
-    padding: 2vh;
+    padding: 1rem;
     margin: 5px;
     background-color: rgb(84, 134, 84);
     color: white;
     border: 2px solid;
     border-radius: 10px;   
-    font-size: 1.25rem;
+    font-size: 1.25rem;   
+
 }
-.about-me, .source-code{
-    width: 300px;
-    height: 25px;
-    padding: 2vh;
-    margin: 5px;
-    background-color: rgb(84, 134, 84);
-    color: white;
-    text-decoration: none;
-    border: 2px solid;
-    border-radius: 10px;   
-    font-size: 1.25rem;
-}
-.about-me:hover, .source-code:hover, .new-game:hover{
+
+.menu-option:hover{
     background-color: rgb(116, 177, 116);
     border: 2px rgba(255, 255, 255, 0.267) solid;
 }  
 
-
-.new-game a, .about-me a, .source-code a{
+.menu-option a{
     color: white;
     text-decoration: none;
 }
+
+.menu-option .dropdown button {
+    border: none;
+    background-color: inherit;
+    outline: none;
+    color: white;
+    font: inherit;
+    width: inherit;
+    height: inherit;
+}
+
+
+.dropdown-content {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    
+    opacity: 0;
+    transition: opacity 400ms;
+}
+
+.active {
+    opacity: 1;
+}
+
+
+
+
+
+
+
+
+
+
+
 </style>
