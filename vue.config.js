74ef2a008f0b7d05eb5202801665cd33e97e8dba
Seth Aker <saker132@gmail.com>
2023-09-22 11:29:58 -0500

    Retrying Dist Setup

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: '/cardgames-webapp'
+  publicPath: '/cardgames-webapp/'
 })
