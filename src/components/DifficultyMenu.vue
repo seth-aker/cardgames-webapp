e5fe4961b510668cef5c46413408ef63329b25ed
saker132 <saker132@gmail.com>
2025-07-13 12:50:21 -0400

    In the middle of postedit changes

--- src/components/DifficultyMenu.vue
+++ src/components/DifficultyMenu.vue
@@ -2,13 +2,18 @@
   <div class="fixed h-full w-full z-10 bg-black/50 flex justify-center items-start overflow-hidden">
     <div class="relative flex flex-col border-2 border-white bg-green-600 top-48 w-80 p-4 text-center">
       <h2>Select Difficulty</h2>
-      <button @click="resumeGame"
-        class="text-white no-underline border-2 border-white rounded-xl py-2 px-3 my-2 cursor-pointer block hover:bg-green-500 hover:border-white/25">Resume
-        Game</button>
-      <button @click="startNewGame"
-        class="text-white no-underline border-2 border-white rounded-xl py-2 px-3 my-2 cursor-pointer block hover:bg-green-500 hover:border-white/25">New
-        Game</button>
-
+      <button @click="startNewGame('easy')"
+        class="text-white no-underline border-2 border-white rounded-xl py-2 px-3 my-2 cursor-pointer block hover:bg-green-500 hover:border-white/25">
+        Easy
+      </button>
+      <button @click="startNewGame('medium')"
+        class="text-white no-underline border-2 border-white rounded-xl py-2 px-3 my-2 cursor-pointer block hover:bg-green-500 hover:border-white/25">
+        Medium
+      </button>
+      <button @click="startNewGame('hard')"
+        class="text-white no-underline border-2 border-white rounded-xl py-2 px-3 my-2 cursor-pointer block hover:bg-green-500 hover:border-white/25">
+        Hard
+      </button>
     </div>
   </div>
 </template>
